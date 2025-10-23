8d8591657e774a74c608f6e5461376cfb41a5727
Simon Schneeberger <simon.schneeberger@ymc.ch>
2025-10-23 11:04:07 +0400

    add missing migration file

---
+++ app/migrations/Version20251023070332.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251023070332 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX consultant_name_unique ON consultant');
+        $this->addSql('ALTER TABLE consultant ADD email VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE consultant DROP email');
+        $this->addSql('CREATE UNIQUE INDEX consultant_name_unique ON consultant (name)');
+    }
+}
